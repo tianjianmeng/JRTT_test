e9b70c055679dd9359b101e2719eb3f6dcd92e2f
扭曲树黑色精子 <1923215589@qq.com>
2021-10-21 14:10:49 +0800

    3

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,50 +1,28 @@
 <template>
   <div class="login-container">
     <!-- 导航栏 -->
-    <van-nav-bar
-      class="app-nav-bar"
-      title="登录"
-      left-arrow
-      @click-left="$router.back()"
-    />
+    <van-nav-bar class="app-nav-bar" title="登录" left-arrow @click-left="$router.back()" />
     <!-- /导航栏 -->
 
     <!-- 登录表单 -->
     <van-cell-group>
-      <van-field
-        v-model="user.mobile"
-        required
-        clearable
-        icon-prefix="toutiao"
-        left-icon="shouji"
-        label="手机号"
-        placeholder="请输入手机号"
-      />
-      <van-field
-        v-model="user.code"
-        clearable
-        icon-prefix="toutiao"
-        left-icon="yanzhengma"
-        label="验证码"
-        placeholder="请输入验证码"
-      >
+      <van-field v-model="user.mobile" required clearable icon-prefix="toutiao" left-icon="shouji" label="手机号"
+        placeholder="请输入手机号" />
+      <van-field v-model="user.code" clearable icon-prefix="toutiao" left-icon="yanzhengma" label="验证码" placeholder="请输入验证码">
         <template #button>
-          <van-button
-            class="send-btn"
-            size="mini"
-            round
-          >发送验证码</van-button>
+          <van-button class="send-btn" size="mini" round>发送验证码</van-button>
         </template>
       </van-field>
     </van-cell-group>
     <div class="login-btn-wrap">
-      <van-button class="login-btn" type="info" block>登录</van-button>
+      <van-button class="login-btn" type="info" block @click="onLogin">登录</van-button>
     </div>
     <!-- /登录表单 -->
   </div>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
 export default {
   name: 'LoginIndex',
   components: {},
@@ -62,25 +40,20 @@ export default {
   created () {},
   mounted () {},
   methods: {
+    ...mapMutations([
+      'setUser'
+    ]),
 
-    async onLogin () {
+    onLogin () {
       this.$toast.loading({
         message: '登录中...', // 提示文本
         forbidClick: true, // 禁止背景点击
         duration: 0 // 展示时长(ms)，值为 0 时，toast 不会消失
       })
-      // 1. 找到数据接口
-      // 2. 封装请求方法
-      // 3. 请求调用登录
       try {
-        // const { data } = await login(this.user)
-
-        // 4. 处理响应结果
         this.$toast.success('登录成功')
-        // +
-
-        // 将后端返回的用户登录状态（token等数据）放到 Vuex 容器中
-        // this.$store.commit('setUser', data.data)
+        this.setUser(this.user.mobile)
+        this.$router.push('/my')
       } catch (err) {
         console.log(err)
         this.$toast.fail('登录失败，手机号或验证码错误')
@@ -91,25 +64,29 @@ export default {
 </script>
 
 <style scoped lang="less">
-.login-container {
-  .send-btn {
-    width: 76px;
-    height: 23px;
-    background-color: #ededed;
-    .van-button__text {
-      font-size: 11px;
-      color: #666666;
-    }
-  }
-  .login-btn-wrap {
-    padding: 26px 16px;
-    .login-btn {
-      background-color: #6db4fb;
-      border: none;
+  .login-container {
+    .send-btn {
+      width: 76px;
+      height: 23px;
+      background-color: #ededed;
+
       .van-button__text {
-        font-size: 15px;
+        font-size: 11px;
+        color: #666666;
+      }
+    }
+
+    .login-btn-wrap {
+      padding: 26px 16px;
+
+      .login-btn {
+        background-color: #6db4fb;
+        border: none;
+
+        .van-button__text {
+          font-size: 15px;
+        }
       }
     }
   }
-}
 </style>
